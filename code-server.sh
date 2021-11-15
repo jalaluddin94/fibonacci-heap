8ef411238db8c8bc25947873fe835cf1fc189b8b
jalaluddin94 <jalaluddin.94@yahoo.com>
2021-11-14 20:35:23 -0500

    change code-server run script

--- code-server.sh
+++ code-server.sh
@@ -1,1 +1,2 @@
-/bin/code-server/code-server --host 0.0.0.0 --port 7071 --auth none
+#/bin/code-server/code-server --host 0.0.0.0 --port 7071 --auth none
+code-server --host 0.0.0.0 --port 7071 --auth none
