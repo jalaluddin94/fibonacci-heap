7e735b1f313781203eba9706e8d4c2d9fbe7e838
jalaluddin94 <jalaluddin.94@yahoo.com>
2021-11-18 09:40:46 -0500

    Card

--- frontend-react/src/App.js
+++ frontend-react/src/App.js
@@ -1,19 +1,36 @@
 import React from 'react';
-import Grid from '@mui/material/Grid';
-import CustomCard from './components/Cards';
+import Avatar from '@mui/material/Avatar';
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
+
+import IconButton from '@mui/material/IconButton';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+
 import logo from './images/kaba.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Grid container spacing={2}>
-        <Grid item xs={2}></Grid>
-        <Grid item xs={8}>
-          <img src={logo} alt="logo" />
-        </Grid>
-        <Grid item xs={2}></Grid>
-      </Grid>
+      <div className="App-header">
+        <Card sx={{ maxWidth: 345 }}>
+          <CardHeader
+            avatar={
+              <Avatar src={logo} alt="logo" sx={{ width: 120, height: 110 }} />
+            }
+            action={
+              <IconButton aria-label="settings">
+                <MoreVertIcon />
+              </IconButton>
+            }
+            title="Fibonacci Heap"
+            subheader="23521059 - 23521089"
+          />
+        </Card>
+      </div>
     </div>
   );
 }
