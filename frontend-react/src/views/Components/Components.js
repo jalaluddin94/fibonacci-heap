646d94b8cbae8e1ce8b055a84a472892a792c6d2
jalaluddin94 <jalaluddin.94@yahoo.com>
2021-11-20 07:40:47 -0500

    change appearance front

--- frontend-react/src/views/Components/Components.js
+++ frontend-react/src/views/Components/Components.js
@@ -2,7 +2,7 @@ import React from "react";
 // nodejs library that concatenates classes
 import classNames from "classnames";
 // react components for routing our app without refresh
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 // @material-ui/core components
 import { makeStyles } from "@material-ui/core/styles";
 // @material-ui/icons
@@ -11,51 +11,59 @@ import Header from "components/Header/Header.js";
 import Footer from "components/Footer/Footer.js";
 import GridContainer from "components/Grid/GridContainer.js";
 import GridItem from "components/Grid/GridItem.js";
-import Button from "components/CustomButtons/Button.js";
+// import Button from "components/CustomButtons/Button.js";
+import Card from "components/Card/Card.js";
+// import CardBody from "components/Card/CardBody.js";
+import CardHeader from "components/Card/CardHeader.js";
+// import CardFooter from "components/Card/CardFooter.js";
 import Parallax from "components/Parallax/Parallax.js";
 // sections for this page
 import HeaderLinks from "components/Header/HeaderLinks.js";
-import SectionBasics from "./Sections/SectionBasics.js";
-import SectionNavbars from "./Sections/SectionNavbars.js";
-import SectionTabs from "./Sections/SectionTabs.js";
-import SectionPills from "./Sections/SectionPills.js";
-import SectionNotifications from "./Sections/SectionNotifications.js";
-import SectionTypography from "./Sections/SectionTypography.js";
-import SectionJavascript from "./Sections/SectionJavascript.js";
-import SectionCarousel from "./Sections/SectionCarousel.js";
-import SectionCompletedExamples from "./Sections/SectionCompletedExamples.js";
-import SectionLogin from "./Sections/SectionLogin.js";
-import SectionExamples from "./Sections/SectionExamples.js";
-import SectionDownload from "./Sections/SectionDownload.js";
+// import SectionBasics from "./Sections/SectionBasics.js";
+// import SectionNavbars from "./Sections/SectionNavbars.js";
+// import SectionTabs from "./Sections/SectionTabs.js";
+// import SectionPills from "./Sections/SectionPills.js";
+// import SectionNotifications from "./Sections/SectionNotifications.js";
+// import SectionTypography from "./Sections/SectionTypography.js";
+// import SectionJavascript from "./Sections/SectionJavascript.js";
+// import SectionCarousel from "./Sections/SectionCarousel.js";
+// import SectionCompletedExamples from "./Sections/SectionCompletedExamples.js";
+// import SectionLogin from "./Sections/SectionLogin.js";
+// import SectionExamples from "./Sections/SectionExamples.js";
+// import SectionDownload from "./Sections/SectionDownload.js";
 
 import styles from "assets/jss/material-kit-react/views/components.js";
 
 const useStyles = makeStyles(styles);
 
 export default function Components(props) {
+  const [cardAnimaton, setCardAnimation] = React.useState("cardHidden");
+  setTimeout(function () {
+    setCardAnimation("");
+  }, 700);
   const classes = useStyles();
   const { ...rest } = props;
   return (
     <div>
       <Header
-        brand="Material Kit React"
+        brand="Fibonacci Heaps"
         rightLinks={<HeaderLinks />}
         fixed
         color="transparent"
-        changeColorOnScroll={{
-          height: 400,
-          color: "white",
-        }}
+        // changeColorOnScroll={{
+        //   height: 400,
+        //   color: "white",
+        // }}
         {...rest}
       />
-      <Parallax image={require("assets/img/bg4.jpg").default}>
+      <Parallax image={require("assets/img/kaba.jpg").default}>
         <div className={classes.container}>
           <GridContainer>
             <GridItem>
               <div className={classes.brand}>
-                <h1 className={classes.title}>Material Kit React.</h1>
+                <h1 className={classes.title}>Administrasi Sistem Haji</h1>
                 <h3 className={classes.subtitle}>
-                  A Badass Material-UI Kit based on Material Design.
+                  23521059 Jalaluddin - 23521089 Aaz M. Hafidh
                 </h3>
               </div>
             </GridItem>
@@ -64,7 +72,7 @@ export default function Components(props) {
       </Parallax>
 
       <div className={classNames(classes.main, classes.mainRaised)}>
-        <SectionBasics />
+        {/* <SectionBasics />
         <SectionNavbars />
         <SectionTabs />
         <SectionPills />
@@ -82,7 +90,20 @@ export default function Components(props) {
           </Link>
         </GridItem>
         <SectionExamples />
-        <SectionDownload />
+        <SectionDownload /> */}
+        <div className={classes.container}>
+          <GridContainer justify="center">
+            <GridItem xs={12} sm={12} md={4}>
+              <Card className={classes[cardAnimaton]}>
+                <form className={classes.form}>
+                  <CardHeader color="primary" className={classes.cardHeader}>
+                    <h4>Login</h4>
+                  </CardHeader>
+                </form>
+              </Card>
+            </GridItem>
+          </GridContainer>
+        </div>
       </div>
       <Footer />
     </div>
